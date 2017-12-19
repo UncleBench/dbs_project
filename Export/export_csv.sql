f9a142b5492c9f6fad8b38bf69e2b2c88c933289
fabmeyer <fabian@meyerfoto.ch>
2017-12-19 17:32:41 +0100

    Pruefe Join Added
    works!

--- Export/export_csv.sql
+++ Export/export_csv.sql
@@ -1,12 +1,12 @@
-COPY (SELECT * FROM assistenten) TO '/private/tmp/export/assistenten.csv' WITH CSV header;
-COPY (SELECT * FROM professoren) TO '/private/tmp/export/professoren.csv' WITH CSV header;
-COPY (SELECT * FROM studenten) TO '/private/tmp/export/studenten.csv' WITH CSV header;
+COPY (SELECT * FROM assistenten) TO '/tmp/assistenten.csv' WITH CSV header;
+COPY (SELECT * FROM professoren) TO '/tmp/professoren.csv' WITH CSV header;
+COPY (SELECT * FROM studenten) TO '/tmp/studenten.csv' WITH CSV header;
+COPY (SELECT * FROM pruefen
+	LEFT OUTER JOIN studenten ON studenten.matrnr = pruefen.matrnr
+	LEFT OUTER JOIN vorlesungen ON vorlesungen.VorlNr = pruefen.vorlnr
+	LEFT OUTER JOIN professoren ON professoren.PersNr = pruefen.persnr
+) TO '/tmp/pruefen.csv' WITH CSV header;
 COPY (SELECT * FROM vorlesungen
 	LEFT OUTER JOIN voraussetzen ON voraussetzen.Vorgänger = vorlesungen.VorlNr
 	LEFT OUTER JOIN voraussetzen ON voraussetzen.Nachfolger = vorlesungen.VorlNr
-	) TO '/private/tmp/export/vorlesungen.csv' WITH CSV header;
-COPY (SELECT * FROM prüfen
-	LEFT OUTER JOIN studenten ON studenten.Legi = prüfen.Legi
-	LEFT OUTER JOIN vorlesungen ON vorlesungen.VorlNr = prüfen.Nr
-	LEFT OUTER JOIN professoren ON professoren.PersNr = prüfen.PersNr
-	) TO '/private/tmp/export/pruefen.csv' WITH CSV header;
+) TO '/tmp/vorlesungen.csv' WITH CSV header;
