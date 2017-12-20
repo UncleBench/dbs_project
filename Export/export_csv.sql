38c2f265c66a74de8fa0443cff7450cd7d82ff83
UncleBench <sali_vali@hotmail.com>
2017-12-20 13:26:19 +0100

    removed all joins on SQL export script

--- Export/export_csv.sql
+++ Export/export_csv.sql
@@ -3,12 +3,5 @@ COPY (SELECT * FROM hören) TO '/tmp/hoeren.csv' WITH CSV header;
 COPY (SELECT * FROM assistenten) TO '/tmp/assistenten.csv' WITH CSV header;
 COPY (SELECT * FROM professoren) TO '/tmp/professoren.csv' WITH CSV header;
 COPY (SELECT * FROM studenten) TO '/tmp/studenten.csv' WITH CSV header;
-COPY (SELECT * FROM pruefen
-	LEFT OUTER JOIN studenten ON studenten.matrnr = pruefen.matrnr
-	LEFT OUTER JOIN vorlesungen ON vorlesungen.VorlNr = pruefen.vorlnr
-	LEFT OUTER JOIN professoren ON professoren.PersNr = pruefen.persnr
-) TO '/tmp/pruefen.csv' WITH CSV header;
-COPY (SELECT * FROM vorlesungen
-	LEFT OUTER JOIN voraussetzen ON voraussetzen.Vorgänger = vorlesungen.VorlNr
-	LEFT OUTER JOIN voraussetzen ON voraussetzen.Nachfolger = vorlesungen.VorlNr
-) TO '/tmp/vorlesungen.csv' WITH CSV header;
+COPY (SELECT * FROM pruefen) TO '/tmp/pruefen.csv' WITH CSV header;
+COPY (SELECT * FROM vorlesungen) TO '/tmp/vorlesungen.csv' WITH CSV header;
