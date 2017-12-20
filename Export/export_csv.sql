8e2c8f9bbee38700b9953116a0ba70405c0dd03c
fabmeyer <fabian@meyerfoto.ch>
2017-12-20 11:06:16 +0100

    Added export / import voraussetzen

--- Export/export_csv.sql
+++ Export/export_csv.sql
@@ -1,4 +1,5 @@
-COPY (SELECT * FROM hören) TO '/tmp/hoeren.csv’ WITH CSV header;
+COPY (SELECT * FROM voraussetzen) TO '/tmp/voraussetzen.csv' WITH CSV header;
+COPY (SELECT * FROM hören) TO '/tmp/hoeren.csv' WITH CSV header;
 COPY (SELECT * FROM assistenten) TO '/tmp/assistenten.csv' WITH CSV header;
 COPY (SELECT * FROM professoren) TO '/tmp/professoren.csv' WITH CSV header;
 COPY (SELECT * FROM studenten) TO '/tmp/studenten.csv' WITH CSV header;
